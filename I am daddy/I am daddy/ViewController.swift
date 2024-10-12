c7d2108b6deeef222e7f2ca892b97b5143042e37
Alan Moura <alanmoura1908@gmail.com>
2024-10-12 21:37:07 +0100

    app. I-am-daddy

---
+++ I am daddy/I am daddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am daddy
+//
+//  Created by Allan Moura on 10/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
